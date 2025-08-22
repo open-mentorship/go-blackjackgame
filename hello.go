a2c7df0ef2d6b33739e030b1a49f96dfb74b9822
ritoban23 <ankudutt101@gmail.com>
2025-08-22 21:53:32 +0530

    Refactor Hello function to accept a language parameter and update test cases accordingly

--- hello.go
+++ hello.go
@@ -3,14 +3,27 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello "
+const spanishHelloPrefix = "Hola "
+const frenchHelloPrefix = "Ca va "
+const spanish = "spanish"
+const french = "French"
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if len(name) == 0 {
 		name = "world"
 	}
-	return englishHelloPrefix + name + "!"
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
