e0820082b042341a5fef5ab5ab6425c3c822c0c2
ritoban23 <ankudutt101@gmail.com>
2025-08-22 12:14:45 +0530

    Refactor Hello function and its test cases for improved clarity and structure

--- hello.go
+++ hello.go
@@ -5,6 +5,9 @@ import "fmt"
 const englishHelloPrefix = "Hello "
 
 func Hello(name string) string {
+	if len(name) == 0 {
+		name = "world"
+	}
 	return englishHelloPrefix + name + "!"
 }
 
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("world")
-	want := "Hello world!"
+	t.Run("Greeting people", func(t *testing.T) {
+		got := Hello("world")
+		want := "Hello world!"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q but want %q", got, want)
+		}
+	})
+	t.Run("Greet 'Hello world!' when empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello world!"
+
+		if got != want {
+			t.Errorf("got %q but want %q", got, want)
+		}
+	})
 }
